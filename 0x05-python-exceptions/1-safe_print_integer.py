4a5c7fc4b039b108b221ec1a2fc458ad1bfc5990
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2022-01-17 20:23:42 +0100

    safe printing of an integers list

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -5,4 +5,4 @@ def safe_print_integer(value):
         return True
     except:
         return False
-                                                                                        
+                                                                          
