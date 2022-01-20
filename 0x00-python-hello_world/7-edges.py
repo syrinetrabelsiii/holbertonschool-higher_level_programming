e7191861c8f676b8e7a0ab241594578a314f8288
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2022-01-20 10:15:25 +0100

    copy cat and paste

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+wf = word[:3]
+wl = word[-2:]
+mw = word[1: -1]
+print("First 3 letters: {}".format(wf))
+print("Last 2 letters: {}".format(wl))
+print("Middle word: {}".format(mw))
