cacc499bb2d8fe81a8c933502da232acaa119413
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2022-01-28 23:04:26 +0100

    adds 2 integers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,8 +1,6 @@
  #!/usr/bin/python3
 """This module defines the add integer function
 """
-
-
 def add_integer(a, b=98):
     """ Function that adds two string.
     Args:
@@ -16,4 +14,3 @@ def add_integer(a, b=98):
     if type(b) != int and type(b) != float or b != b:
         raise TypeError("b must be an integer")
     return int(a) + int(b)
- 
