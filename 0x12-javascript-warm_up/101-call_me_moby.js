039095dc41780e1d1e70a2afb49ee5d0a770cb4d
syrinetrabelsiii <syrinetrabelsi99@gmail.com>
2022-08-01 23:30:43 +0100

    call me Moby

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
-exports.addMeMaybe = (x, theFunction) => {
-  theFunction(++x);
-};
+exports.callMeMoby = function (x, theFunction) {
+    for (let i = 0; i < x; i++) {
+      theFunction();
+    }
+  };
